11139e3fdabca55162389cc49015b9a72f48aacd
Johnny Ho <johnnyho@uchicago.edu>
2015-06-25 14:51:08 -0500

    Use the Flask-SQLAlchemy extension.

--- lhcdb/database.py
+++ lhcdb/database.py
@@ -1,16 +1,30 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from flask.ext.sqlalchemy import SQLAlchemy
 
-engine = create_engine('postgresql://localhost/lhcdb', echo=False)
+from lhcdb import app
 
-db_session = scoped_session(
-    sessionmaker(autocommit=False, autoflush=False, bind=engine)
-    )
+db = SQLAlchemy(app)
 
-Base = declarative_base()
-Base.query = db_session.query_property()
+db_session = db.session
+
+Base = db.Model
 
 def init_db():
     import lhcdb.models
-    Base.metadata.create_all(bind=engine)
+    db.create_all()
+
+#from sqlalchemy import create_engine
+#from sqlalchemy.orm import scoped_session, sessionmaker
+#from sqlalchemy.ext.declarative import declarative_base
+#
+#engine = create_engine('postgresql://localhost/lhcdb', echo=False)
+#
+#db_session = scoped_session(
+#    sessionmaker(autocommit=False, autoflush=False, bind=engine)
+#    )
+#
+#Base = declarative_base()
+#Base.query = db_session.query_property()
+#
+#def init_db():
+#    import lhcdb.models
+#    Base.metadata.create_all(bind=engine)
